2abdd876311a1f1f9909cc6748d1ff8d5eec80c6
pashpashpash <nik@nugbase.com>
2024-07-08 17:32:25 +0100

    cleanup

--- routers/retrieve.py
+++ routers/retrieve.py
@@ -1,25 +1,26 @@
 from tenacity import retry, wait_random_exponential, stop_after_attempt, retry_if_not_exception_type
-from typing import List, Dict, Any
-from fastapi import APIRouter, Form
 from openai import OpenAI, BadRequestError
+from fastapi import APIRouter, Form
+from typing import List, Dict, Any
 from dataclasses import dataclass
+
 import turbopuffer as tpuf
 import cohere
 import os
 
-router = APIRouter()
 client = OpenAI()
+router = APIRouter()
 tpuf.api_key = os.getenv("TURBOPUFFER_API_KEY")
 co = cohere.Client(api_key=os.getenv("COHERE_API_KEY"))
 
 @dataclass
 class Retrieval:
-    text: str                   # The text of the chunk
-    embedding: List[float]       # The embedding of the chunk
-    vectorID: str               # The unique vectorID of the chunk, to identify it in vectordb
-    fileID: str                  # A unique fileID of the chunk, for relational db purposes
-    cosine_similarity_score: float              # Cosine similarity score
-    reranker_score: float        # Reranker relevance score
+    text: str                          # The text of the chunk
+    embedding: List[float]              # The embedding of the chunk
+    vectorID: str                      # The unique vectorID of the chunk, to identify it in vectordb
+    fileID: str                         # A unique fileID of the chunk, for relational db purposes
+    cosine_similarity_score: float      # Cosine similarity score - Important: Make sure you store this for observability purposes 
+    reranker_score: float               # Reranker relevance score - Important: Make sure you store this for observability purposes 
 
 
 @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6), retry=retry_if_not_exception_type(BadRequestError))
@@ -42,17 +43,13 @@ async def get_context(queries: List[str] = Form(...), namespace: str = Form(...)
             top_k=10,
             include_attributes=['text', 'fileID']
         )
-        
-        # Prepare documents for reranking
+
+        # Step 3: Rerank chunks + store reranker score
         docs = [vector.attributes["text"] for vector in vectors]
-        
         if not docs:
             response.append([])
             continue
-
-        # Step 3: Rerank chunks + store reranker score
-        rerank_results = co.rerank(model="rerank-english-v3.0", query=query, documents=docs, top_n=10, return_documents=True)
-        
+        rerank_results = co.rerank(model="rerank-multilingual-v3.0", query=query, documents=docs, top_n=10, return_documents=True)
         retrieved_chunks: List[Retrieval] = []
         for idx, result in enumerate(rerank_results.results):
             retrieved_chunks.append(Retrieval(
